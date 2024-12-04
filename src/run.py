734307e2a0f857b0182f1f6e4a34db7d68d6cea6
drhobot <andy@impv.au>
2024-12-04 18:05:23 +1100

    cleanup

--- src/run.py
+++ src/run.py
@@ -3,20 +3,16 @@ import os
 
 from settings import *
 
-def run():
+for experiment in get_experiements():
 
-    for experiment in get_experiements():
+    os.makedirs(experiment["out"], exist_ok=True)
+    
+    fullCmd = (
+        f'{cmd} '
+        f'-s {experiment["seed"]} '
+        f'-t {experiment["train"]} '
+        f'-T {experiment["test"]} '
+        f'-lt {experiment["out"]} '
+    )
 
-        os.makedirs(experiment["out"], exist_ok=True)
-        
-        fullCmd = (
-            f'{cmd} '
-            f'-s {experiment["seed"]} '
-            f'-t {experiment["train"]} '
-            f'-T {experiment["test"]} '
-            f'-lt {experiment["out"]} '
-        )
-
-        os.system(fullCmd)
-
-run()
+    os.system(fullCmd)
